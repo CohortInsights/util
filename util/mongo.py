dbfa752b222cc2e5c49d5695af16a8a9e2ea52ec
Roger Chylla <roger.chylla@gmail.com>
2024-01-18 10:28:02 -0600

    PyProject and lock updates

--- util/mongo.py
+++ util/mongo.py
@@ -1,4 +1,6 @@
 from pymongo.mongo_client import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
 
 import traceback
 from util.log import LogEvent
@@ -41,3 +43,89 @@ def connect(uri : str, user : str, password : str, ping_test = True) -> MongoCli
         client._info = (client.close, event, info)
         client.close = close_and_log    # Override native close method with close_and_log
     return client
+
+
+def replace_values(expression : dict, map : dict, key_list : list[str] = None) -> dict:
+    """
+    Replace all keys in the input expression with their corresponding map values
+    Args:
+        expression: Input dictionary
+        map: Map of keys and values
+        key_list: Optional selected list of keys from the map
+
+    Returns:
+        Expression with all @key tokens replaced with their corresponding values from the map
+    """
+    if key_list is None:
+        key_list = map.keys()
+
+    for k in key_list:
+        value = map.get(k)
+        if value:
+            expression[k] = value
+    return expression
+
+
+def get_parameter(operation : dict, name : str, meta_data : dict = None, default_value = None, error_check = True):
+    value = operation.get(name)
+    if not value and meta_data:
+        value = meta_data.get(value)
+    if not value and default_value:
+        value = default_value
+    if not value and error_check:
+        message = str.join("No ",name, " present in ",operation," or meta data")
+        raise Exception(message)
+    return value
+
+
+class MongoDB:
+    """
+    Wrapper around a Mongo Database that uses meta data to provide store and query operations
+    """
+    def __init__(self, client : MongoClient, meta_data : dict):
+        """
+        Constructor
+
+        Args:
+            database : Instance of a Mongo database
+            meta_data: Meta data used to form queries, filters, and configured operations
+        """
+        self.client = client
+        self.meta_data = meta_data
+
+    def get_store_callback(self, name : str):
+        """
+        Gets a callback function that stores an item in the database
+
+        Args:
+            name: Name of store operation derived from meta data
+
+        Returns:
+            Function that stores an item in the underlying DB
+        """
+        operations = self.meta_data.get('operations')
+        store_oper = operations.get(name)
+        if store_oper is None:
+            return None
+
+        database_name = get_parameter(store_oper, 'database', meta_data=self.meta_data)
+        collection_name = get_parameter(store_oper, 'collection', meta_data=self.meta_data)
+        collection : Collection = self.client.get_database(database_name).get_collection(collection_name)
+        if collection is None:
+            return None
+
+        key_list = get_parameter(store_oper, 'key_list')
+        filter = get_parameter(store_oper, 'filter')
+        upsert = get_parameter(store_oper, 'upsert', default_value=True)
+
+        def store_value(item : dict):
+            if filter:
+                _filter = replace_values(filter, item, key_list)
+                return collection.replace_one(filter=_filter, replacement=item, upsert=upsert)
+            else:
+                return collection.insert_one(item)
+
+        return store_value
+
+
+
